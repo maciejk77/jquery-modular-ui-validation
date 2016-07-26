f53b9ba0831222332eccb9a4d974f316875d4bd8
Maciej Kurek <maciejk77@gmail.com>
2016-07-26 12:12:31 +0100

    Test completed

--- js/script.js
+++ js/script.js
@@ -19,20 +19,21 @@ var app = app || {};
 			$('#login form').submit(function(event) {
 	
 				//Email and password validation
-      	if (!(app.validation.email($("#email").val()) && app.validation.password($("#id").val()))) {
+      			if (!(app.validation.email($("#email").val()) && app.validation.password($("#id").val()))) {
 
-	      	//Preventing default event clickthgrough if validation above not successful
-        	event.preventDefault();
+		      	//Preventing default event clickthgrough if validation above not successful
+	        	event.preventDefault();
+	        	$(".email-error").css("display", "none");
     
-       		//Email error message to appear if incorrect email address provided 
-          if(!app.validation.email($("#email").val())){
-            app.errorMsg.email();
-          };
+       			//Email error message to appear if incorrect email address provided 
+	         	if(!app.validation.email($("#email").val())){
+	         		app.errorMsg.email();
+	         	}
 
-          //Password error message to appear if incorrect password provided 
-          if(!app.validation.password($("#id").val())) {
-            app.errorMsg.password();
-          };
+		        //Password error message to appear if incorrect password provided 
+		        if(!app.validation.password($("#id").val())) {
+		        	app.errorMsg.password();
+		        };
 				};
 			});
 		}
@@ -40,30 +41,24 @@ var app = app || {};
 	app.validation = {
 		email: function(email) {
 			// Regex, use this to validate the Email. It return true or false.
-
 			var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 			return re.test(email);
 		},
 		password: function(id) {
 			// Use this to validate the Password. Checks if value is empty. It return true or false.
-
-			var elementVal = $.trim(id.val());
+			var elementVal = $.trim(id);
 			if(elementVal.length > 0)
 				return true;
 		}
 	},
+	
 	app.errorMsg = {
 		email: function(id) {
-			var text = 'Your E-Mail is not valid';
-			//Appending new paragraph element with text provided in variable to target div
-			$(".email-error-msg").append("<p class='email-error'>" + text + "</p>");
-
+			$(".email-error").fadeIn("slow");
 		},
-		password: function(id) {
-			var text = 'Your Password is not valid';
-			//Appending new paragraph element with text provided in variable to target div 
-			$(".password-error-msg").append("<p class='password-error'>" + text + "</p>");
 
+		password: function(id) {
+			$(".password-error").fadeIn("slow");
 		}
 	}
 	app.docOnReady = {
